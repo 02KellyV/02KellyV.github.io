060d1cd5826a94e0ebfd5f7d333edb5f686d43cd
KellySuperSaya <kellyssj02@gmail.com>
2019-03-18 11:18:14 -0500

    compilehv

---
+++ precache-manifest.40aa5100a9297c07353ea6d0445fbb75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79eefc23f6c60f5a8a83e269db78f45a",
+    "url": "/static/media/banner-bg.79eefc23.png"
+  },
+  {
+    "revision": "4a1e62a76c4935fe419d4b0e90f38e14",
+    "url": "/static/media/FotoAVATAR-01.4a1e62a7.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2d5f48459827df22bc8a",
+    "url": "/static/js/main.2d5f4845.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "2d5f48459827df22bc8a",
+    "url": "/static/css/main.496c6681.chunk.css"
+  },
+  {
+    "revision": "1777163c45656246a92f82ce1a00db05",
+    "url": "/index.html"
+  }
+];
